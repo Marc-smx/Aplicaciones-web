b6942e54297e6af389426f2331c4caf1df80c8f6
Marc-smx <marortper11@alu.edu.gva.es>
2026-02-25 17:00:37 +0100

    Add files via upload

---
+++ Javascript/semaforo/script.js
@@ -0,0 +1,28 @@
+function ponerRojo() {
+    document.getElementById("rojo").style.backgroundColor = "red";
+    document.getElementById("amarillo").style.backgroundColor = "gray";
+    document.getElementById("verde").style.backgroundColor = "gray";
+    document.getElementById("titulo").innerHTML = "LUZ ROJA";  
+}
+    function ponerAmarillo() {
+    document.getElementById("rojo").style.backgroundColor = "gray";
+    document.getElementById("amarillo").style.backgroundColor = "yellow";
+    document.getElementById("verde").style.backgroundColor = "gray";
+    document.getElementById("titulo").innerHTML = "LUZ AMARILLA";  
+
+}
+function ponerVerde() {
+    document.getElementById("rojo").style.backgroundColor = "gray";
+    document.getElementById("amarillo").style.backgroundColor = "gray";
+    document.getElementById("verde").style.backgroundColor = "green";
+    document.getElementById("titulo").innerHTML = "LUZ VERDE";  
+}
+
+function ponerTodosApagados() {
+    document.getElementById("rojo").style.backgroundColor = "gray";
+    document.getElementById("amarillo").style.backgroundColor = "gray";
+    document.getElementById("verde").style.backgroundColor = "gray";
+    document.getElementById("titulo").innerHTML = "SEMAFORO APAGADO";  
+
+}
+
